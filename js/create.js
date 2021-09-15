1597a864c988cc600c3013a02b2504ba050d92b5
Mustafa Uzun <mustafauzun0@gmail.com>
2021-09-15 11:59:31 +0300

    Update create.js
    + Url names convert to hash

--- js/create.js
+++ js/create.js
@@ -1,18 +1,23 @@
 window.onload = function() {
 
-	function setClearURL(str) {
-
-		return str.replace(/[^\w\d]/gi, '').substring(0, 30);
-
+	function hashCode(text) {
+		var hash = 0;
+		if (text.length == 0) return hash;
+		for (i = 0; i < text.length; i++) {
+			char = text.charCodeAt(i);
+			hash = ((hash<<5)-hash)+char;
+			hash = hash & hash;
+		}
+		return hash;
 	}
 
 	chrome.tabs.getSelected(null, function(tab) {
 
 		let url = new URL(tab.url);
 
-		let chatURL = setClearURL(url.hostname + url.pathname);
+		let chatURL = hashCode('undefined objects' + url.hostname + url.pathname);
 		
-		document.querySelector('#tlkIO').src = 'https://www.tlk.io/' + chatURL;
+		document.querySelector('#tlkIO').src = 'https://www.tlk.io/' + 'r' + chatURL;
 
 	});
 
